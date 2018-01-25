a7e7aa3cb157aafeae76f68ffa623b72e119771a
FOLIO3PK\hshabbir <hshabbir@folio3.com>
2018-01-25 18:18:00 +0500

    Demo App init for templating using Vue.js and Angular Dynamic Component loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+declare var Vue;
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements AfterViewInit {
   title = 'app';
+  templateHtml = '';
+  constructor() {
+
+  }
+
+
+  ngAfterViewInit() {
+    const vue = new Vue({
+      el: '#iDapp',
+      delimiters: ["<%", "%>"],
+      data: {
+        msg: 'Hello World!',
+        message: '',
+      }
+    });
+
+  }
+
 }
